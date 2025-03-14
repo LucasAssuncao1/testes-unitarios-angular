3096923e982fca1e243a464fe22787f96a46f018
Lucas Araujo <assuncao.araujo.lucas@gmail.com>
2025-03-14 19:06:10 -0300

    testanto a unidade nos metódos e ifs do component

--- src/app/shared/banking/banking.component.spec.ts
+++ src/app/shared/banking/banking.component.spec.ts
@@ -10,7 +10,7 @@ describe('BankingComponent', () => {
     await TestBed.configureTestingModule({
       imports: [BankingComponent]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(BankingComponent);
     component = fixture.componentInstance;
@@ -19,5 +19,47 @@ describe('BankingComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('(U) getPoupanca(): should have = 10', () => {
+    expect(component.getPoupanca()).toEqual(10);
+  });
+
+  it('(U) getCarteira(): should have = 50', () => {
+    expect(component.getCarteira()).toEqual(50);
+  });
+
+  it('(U) setSacar(): should transfer from poupanca to carteira', () => {
+    component.setSacar('10');
+    expect(component.getPoupanca()).toEqual(0);
+    expect(component.getCarteira()).toEqual(60);
+  });
+
+  it(`
+    (U) setSacar(): should transfer from poupanca not allowed string(isNaN)
+     or poupanca < value`, () => {
+    expect(component.setSacar('string')).not.toBeTruthy();
+    expect(component.setSacar('100')).not.toBeTruthy();
+
+    expect(component.getPoupanca()).toEqual(10);
+    expect(component.getCarteira()).toEqual(50);
+
+  });
+
+  it('(U) setDepositar(): should transfer from carteira to poupanca', () => {
+    component.setDepositar('50');
+    expect(component.getCarteira()).toEqual(0);
+    expect(component.getPoupanca()).toEqual(60);
+  });
+
+  it(`
+    (U) setDepositar(): should transfer from carteira not allowed string(isNaN)
+     or carteira < value`, () => {
+    expect(component.setDepositar('string')).not.toBeTruthy();
+    expect(component.setDepositar('100')).not.toBeTruthy();
+
+    expect(component.getPoupanca()).toEqual(10);
+    expect(component.getCarteira()).toEqual(50);
+
   });
 });
