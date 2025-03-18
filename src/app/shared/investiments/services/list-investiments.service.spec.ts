06473900f6e656bb573d6f1298fbcd96686bb7d1
Lucas Araujo <assuncao.araujo.lucas@gmail.com>
2025-03-18 09:14:43 -0300

    finalizando testes unitários e de interface do list.component

--- src/app/shared/investiments/services/list-investiments.service.spec.ts
+++ src/app/shared/investiments/services/list-investiments.service.spec.ts
@@ -5,6 +5,7 @@ import { ListInvestimentsService } from './list-investiments.service';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { HttpClient } from '@angular/common/http';
 import { Investiments } from '../model/investiments';
+import { MOCK_LIST } from './list.investiments.mock';
 
 
 
@@ -15,28 +16,7 @@ describe('ListInvestimentsService', () => {
 
   const url = 'https://raw.githubusercontent.com/troquatte/fake-server/main/investiments-all.json';
 
-  const mockList: Array<Investiments>= [
-    {
-      name: 'Banco 1',
-      value: 100
-    },
-    {
-      name: 'Banco 2',
-      value: 100
-    },
-    {
-      name: 'Banco 3',
-      value: 100
-    },
-    {
-      name: 'Banco 4',
-      value: 100
-    },
-    {
-      name: 'Banco 5',
-      value: 100
-    },
-  ];
+  const mockList: Array<Investiments>= MOCK_LIST;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -65,7 +45,7 @@ describe('ListInvestimentsService', () => {
       const req = httpTestingController.expectOne(url);
       req.flush(mockList);
 
-      expect(req.request.method).toEqual('GET'); 
+      expect(req.request.method).toEqual('GET');
   });
 
 
