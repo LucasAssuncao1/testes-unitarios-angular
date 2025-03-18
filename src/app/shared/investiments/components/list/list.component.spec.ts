877a7c6c4fa3fcf26b1a55173d574def8c69e445
Lucas Araujo <assuncao.araujo.lucas@gmail.com>
2025-03-18 09:14:10 -0300

    terminando test unitarios do service

--- src/app/shared/investiments/components/list/list.component.spec.ts
+++ src/app/shared/investiments/components/list/list.component.spec.ts
@@ -1,11 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ListComponent } from './list.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ListInvestimentsService } from '../../services/list-investiments.service';
+import { MOCK_LIST } from '../../services/list.investiments.mock';
+import { Investiments } from '../../model/investiments';
+import { of } from 'rxjs';
 
 
 describe('ListComponent', () => {
   let component: ListComponent;
   let fixture: ComponentFixture<ListComponent>;
+  let service: ListInvestimentsService;
+
+   const mockList: Array<Investiments>= MOCK_LIST;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -14,6 +21,7 @@ describe('ListComponent', () => {
     .compileComponents();
 
     fixture = TestBed.createComponent(ListComponent);
+    service = TestBed.inject(ListInvestimentsService);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
@@ -23,18 +31,33 @@ describe('ListComponent', () => {
   });
 
   it('(U): should investiments have 4 elements && the first contain = Itaú && the last contain = Nubank', () => {
-    let investiments = component.investiments;
-    expect(investiments.length).toBe(4);
-    expect(investiments[0].name).toContain('Itaú');
-    expect(investiments[3].name).toContain('Nubank');
+    spyOn(service, 'getList').and.returnValue(of(mockList));
+
+    component.ngOnInit();
+    fixture.detectChanges();
+
+    expect(service.getList).toHaveBeenCalledWith();
+
+    expect(component.investiments.length).toEqual(5);
+    expect(component.investiments[0].name).toEqual('Banco 1');
+    expect(component.investiments[4].name).toEqual('Banco 5');
+
+
   });
 
   it('(I): should investiments have 4 elements && the first == Itaú && the last == Nubank', ()=> {
+
+    spyOn(service, 'getList').and.returnValue(of(mockList));
+
+    component.ngOnInit();
+    fixture.detectChanges();
+
     let investiments = fixture.debugElement.nativeElement.querySelectorAll('.list-itens');
 
-    expect(investiments.length).toBe(4);
-    expect(investiments[0].textContent.trim()).toEqual('Itaú | 100');
-    expect(investiments[3].textContent.trim()).toEqual('Nubank | 100');
+
+    expect(investiments.length).toEqual(5);
+    expect(investiments[0].textContent.trim()).toEqual('Banco 1 | 100');
+    expect(investiments[4].textContent.trim()).toEqual('Banco 5 | 100');
   });
+
 });
- 
